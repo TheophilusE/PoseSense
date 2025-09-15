036d311a99ea4f68fccb8a115c31c09a63db43e8
Theophilus Eriata <theoeriata@gmail.com>
2025-09-15 07:51:55 -0600

    Revert "Fix scaling."
    This reverts commit 4bbd6db237b7befd4ecfc7e6a229524b0d8e0703.

--- src/main.ts
+++ src/main.ts
@@ -311,7 +311,7 @@ function normalizeIncomingPose(joints: Joints) {
     const p = new THREE.Vector3(arr[0], arr[1], arr[2]);
 
     // Center at hips and scale to model
-    p.sub(hipCenter).multiplyScalar(smoothedScale * 100);
+    p.sub(hipCenter).multiplyScalar(smoothedScale);
 
     // Axis adjustments
     if (INVERT_Y) p.y *= -1;
@@ -377,8 +377,7 @@ function buildBoneInfo(fbx: THREE.Group) {
 new FBXLoader().load(
   MODEL_PATH,
   (fbx) => {
-    fbx.scale.setScalar(0.1);
-
+    // Keep native FBX scale; do not manually scale the model.
     fbx.traverse((c) => {
       if ((c as THREE.Mesh).isMesh) {
         const m = c as THREE.Mesh;
